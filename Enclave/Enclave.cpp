e1e760f15b95ba90f50f17667ff2de36d17c68d7
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-12-04 13:34:18 +0530

    Read all set instructions and write to global secure store

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -111,33 +111,36 @@ void secure_store(int set_row_count, char **set_instr)
     char *key;
     char *val;
 
-    memset(instr, 0, BUFFER_SZ);
-    memcpy(instr, set_instr[2047], BUFFER_SZ);
-    // print(instr);
-
-    if (strncmp(instr, "SET", 3) == 0)
+    for (int i = 0; i < 2048; i++)
     {
-        keyid = strtok_r(instr + 4, " ", &valnum);
-        // print(keyid);
-        // print(valnum); // should be 1
-        key = new char[strlen(keyid)+1];
-        memset(key, 0, strlen(keyid)+1);
-        memcpy(key, keyid, strlen(keyid));
-        // print(key);
-        
-        val = new char[strlen(valnum)];
-        memset(val, 0, strlen(valnum));
-        memcpy(val, valnum, strlen(valnum)-1);
-        // print(val);
+        memset(instr, 0, BUFFER_SZ);
+        memcpy(instr, set_instr[i], BUFFER_SZ);
+        // print(instr);
 
-        set(atoi(key), atoi(val));
+        if (strncmp(instr, "SET", 3) == 0)
+        {
+            keyid = strtok_r(instr + 4, " ", &valnum);
+            // print(keyid);
+            // print(valnum); // should be 1
+            key = new char[strlen(keyid)+1];
+            memset(key, 0, strlen(keyid)+1);
+            memcpy(key, keyid, strlen(keyid));
+            print(key);
+            
+            val = new char[strlen(valnum)];
+            memset(val, 0, strlen(valnum));
+            memcpy(val, valnum, strlen(valnum)-1);
+            // print(val);
 
-        delete[] key;
-        delete[] val;
-    } else
-    {
-        print("Nothing");
+            set(atoi(key), atoi(val));
+
+            delete[] key;
+            delete[] val;
+        } else
+        {
+            print("Nothing");
+        }
     }
     print("secure_store::Set instructions executed.");
-    // iprint(gst->entries[hash(2047+1)].val);
+    iprint(gst->entries[hash(2047+1)].key);
 }
